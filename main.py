3bce8bcca411def5bad4ac4055945d5c37049aa8
Jacob Szulewski <SzulewskiJacob@gmail.com>
2024-08-17 15:05:13 -0500

    Changes to controls and verbiage

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ client = OpenAI(
 
 st.title("What Should I Watch?")
 
-genres = st.text_input("Which genres sound interesting to you?", placeholder='horror, documentaries, maybe fast-paced')
-streaming_services = st.text_input("Which streaming services do you have access to?", placeholder="Max, Hulu, Netflix, I'd rent something too")
-content_type = st.multiselect(
-    "TV Show? Movie? You can select both.",
-    ["TV Shows", "Movies"]
+genres = st.text_input("Which genres interest you?", placeholder='horror, but less jump scares')
+streaming_services = st.text_input("Which streaming services do you have access to?", placeholder="Max, Hulu")
+content_type = st.selectbox(
+    "Movies, shows, or both?",
+    ["TV Shows", "Movies", "Both"]
 )
-additional_info = st.text_area("Anything else you'd like to share?", placeholder='Ideally something made in the last 10 years. I really like anything A24. Oh and this is for a movie night with college friends.')
+additional_info = st.text_area("Anything else you'd like to share?", placeholder='Ideally something made in the last 10 years. I like A24 movies.')
 
 if st.button("Get Recommendation"):
     genre_str = genres if genres != '' else "any"
     streaming_service_str = streaming_services if streaming_services != "" else "any streaming service"
-    content_type_str = ", ".join(content_type) if content_type else "TV shows or movies"
+    content_type_str = content_type if content_type in ('TV Shows','Movies') else "TV shows or movies"
     
     prompt = f"I like {genre_str} {content_type_str} and I have access to {streaming_service_str}. {additional_info}. Please provide 4 options on what I should watch."
     
